f18b6b06b7e281d9f72504d53eab7260aeeeea17
KazumaProject <59742125+KazumaProject@users.noreply.github.com>
2022-10-19 20:43:41 -0400

    version 22

--- app/src/main/java/com/kazumaproject7/qrcodescanner/other/AppPreferences.kt
+++ app/src/main/java/com/kazumaproject7/qrcodescanner/other/AppPreferences.kt
@@ -7,9 +7,9 @@ import androidx.preference.PreferenceManager
 object AppPreferences {
     private lateinit var preferences: SharedPreferences
 
-    private val MASK_VISIBILITY = Pair("camera_mask_key",false)
+    private val MASK_VISIBILITY = Pair("camera_mask_key",true)
     private val CENTER_CROSS_VISIBILITY = Pair("camera_cross_visibility_key",false)
-    private val HORIZONTAL_LINE_VISIBILITY = Pair("camera_horizontal_line_visibility_key",true)
+    private val HORIZONTAL_LINE_VISIBILITY = Pair("camera_horizontal_line_visibility_key",false)
     private val OPEN_URL_BY_DEFAULT = Pair("result_open_url_by_default_key",true)
     private val OPEN_RESULT_SCREEN = Pair("result_open_result_screen",false)
     private val SHOW_SHARE = Pair("result_open_browser_in_capture",false)
